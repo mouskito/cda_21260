3712dfe6a25a67b8d69d179779f4b5e9d5526758
Moussa Camara <msa.camara@gmail.com>
2022-04-25 16:56:49 +0200

    afpa

--- ajax.js
+++ ajax.js
@@ -12,7 +12,35 @@ $(".nav-item a").click(function () {
 
     //VERSION 2  avec load
 
-    $(".container").load($(this).attr("href") + ".container")
+    $(".container").load($(this).attr("href") + " .container")
+
+    $.ajax({
+          url:"afpa.json",
+          dataType:"json",
+          success:function(data){
+           console.log(data);
+
+        var style = "<ul>";
+           for (var i = 0; i < data.stagiaire.length; i++) {
+            var nom_stagiaire = data.stagiaire[i].nom;
+            var prenom_stagiaire = data.stagiaire[i].prenom;
+            
+            style = style+  "<li>";
+           // style += "<li>";
+            style +=  nom_stagiaire+" ";
+            style += prenom_stagiaire +" ";
+            style += data.stagiaire[i].email;
+            style += "</li> ";
+           }
+           style += "</ul>";
+        $(".ul").html(style)
+          },
+          error:function(xhr) {
+           console.log(xhr.status)
+          }
+         })
+        
+        
 
     return false;
 })
